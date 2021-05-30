10389a1b2d97575c1216081e1fe0313426769718
Philip indra prayitno <philip.prayitno@gmail.com>
2021-05-30 14:17:57 +0700

    adjust: register

--- JS/register.js
+++ JS/register.js
@@ -1,29 +1,11 @@
 $( document ).ready(function(){
 
     $("#btn-register").click(function() {
-        var radioField = document.getElementsByName('account');
-        for (var i = 0; i < radioField.length; i++) {
-            if (radioField[i].checked) {
-                if(radioField[i].value == "pd") {
-                    window.location.href = "home-penggalang-dana.html";
-                } else {
-                    window.location.href = "home-donatur.html";
-                }
-            }
-        }
+        window.location.href = "home-donatur.html";
     });
 
     $("#btn-register-google").click(function() {
-        var radioField = document.getElementsByName('account');
-        for (var i = 0; i < radioField.length; i++) {
-            if (radioField[i].checked) {
-                if(radioField[i].value == "pd") {
-                    window.location.href = "home-penggalang-dana.html";
-                } else {
-                    window.location.href = "home-donatur.html";
-                }
-            }
-        }
+         window.location.href = "home-donatur.html";
     });
 
     $('#avatar').click(function(){ $('#imgupload').trigger('click'); });
