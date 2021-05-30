9ea93fd02ec457ee8d64477d12f197bb5adb5dbb
Philip indra prayitno <philip.prayitno@gmail.com>
2021-05-30 14:28:04 +0700

    feat: notification popup

--- JS/detail.js
+++ JS/detail.js
@@ -14,6 +14,31 @@ $(function() {
         );
     });
 
+    var $tc = $('#tc-1');
+    var $ic = $('#sml-icn1');
+    $tc.click(function() {
+        $backgroundArea.css(
+            {
+                "visibility": "visible"
+            }
+        );
+        $('.agreement-modal-content').css(
+            {
+                "visibility": "visible"
+            }
+        );
+        $ic.css (
+            {
+                "visibility": "hidden"
+            }
+         )
+         $tc.css (
+            {
+                "background-color": "#f9f9f9"
+            }
+         )
+    });
+
     var $backgroundArea = $('.agreement-modal');
     $backgroundArea.click(function() {
         $backgroundArea.css(
